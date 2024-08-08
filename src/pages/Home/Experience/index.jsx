1224fb91c0bb05ab6a93c5d7b6f1a920e644b4ef
Soham Pathak <soham.pathak@datenwissen.com>
2024-08-08 23:20:00 +0530

    Added more code

--- src/pages/Home/Experience/index.jsx
+++ src/pages/Home/Experience/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <div className="w-full flex justify-center items-center pt-20">
+      <div className="w-full flex flex-col max-w-[1240px]">
+        <div className="w-full flex justify-center">
+          <h2 className="text-heading text-5xl">Experience</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
--- src/pages/Home/Hero/index.jsx
+++ src/pages/Home/Hero/index.jsx
@@ -6,18 +6,21 @@ const Hero = () => {
     <div className="w-full flex justify-center items-center pt-20">
       <div className=" w-full flex max-w-[1240px]">
         <div className="flex flex-col w-1/2 mt-20 pt-4">
-          <h2 className="text-heading text-5xl py-5">
-            Get your imagination to reality
+          <h2 className="text-heading flex text-6xl pt-5">
+            Hi, I<span className="text-7xl text-primary">' </span>m Soham
+            <div className="h-5 w-5 rounded-full bg-primary"></div>
           </h2>
-          <p className="text-subheading text-2xl pr-10">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolore
-            mollitia eaque maiores consectetur corporis, ut rerum velit, alias
-            inventore odio vel odit, quae libero dolor unde provident dolorem
-            iste.
+          <h2 className="text-heading text-2xl py-5">
+            I'm a Full Stack Developer & AI Researcher
+          </h2>
+          <p className="text-subheading text-xl pr-10">
+            I've spend the last 3 years building and scaling software for some
+            pretty cool companies. I also teach neural networks to remember
+            things. In case you need developer or a teacher. Let's Connect!
           </p>
           <h2 className="border-4 font-bold text-xl text-primary mt-14 border-primary relative flex items-center justify-center max-w-[250px] py-2 my-5 text-center rounded-lg px-5">
             Get In Touch
-            <img className="absolute -right-12 h-32" src={Kangaroo}/>
+            <img className="absolute -right-12 h-32" src={Kangaroo} />
           </h2>
         </div>
         <div className="flex">
